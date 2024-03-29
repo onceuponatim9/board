d8af18a1dd664e3d376fc576002b16c38ffdd0e1
onceuponatim9 <sjung8018a@gmail.com>
2024-03-29 11:49:22 +0900

    Add join

--- board/src/board/Space.java
+++ board/src/board/Space.java
@@ -8,10 +8,12 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
+
 public class Space {
 	private Scanner scan = new Scanner(System.in);
 	
 	Map<User, Board> map = new HashMap<User, Board>();
+	private UserManager um = new UserManager();
 	
 	private String fileName = "board.txt";
 	
@@ -26,8 +28,39 @@ public class Space {
 		
 	}
 	
-	public void join() {
+	private int inputNumber(String message) {
+		int number = -1;
+		try {
+			System.out.print(message + " : ");
+			String input = scan.next();
+			number = Integer.parseInt(input);
+		} catch(Exception e) {
+			System.out.println("숫자를 입력하세요.");
+		}
 		
+		return number;
+	}
+	
+	private String inputString(String message) {
+		System.out.print(message + " : ");
+		return scan.next();
+	}
+	
+	public void join() {
+		String name = inputString("name");
+		String id = inputString("id");
+		String pw = inputString("pw");
+		
+		String userId = um.findUserId(id);
+		
+		if(userId != null) {
+			System.out.println("이미 존재하는 아이디입니다.");
+			return;
+		}
+		
+		um.createUser(name, id, pw);
+		
+		System.out.println("회원가입 완료");
 	}
 	
 	public void leave() {
@@ -62,13 +95,36 @@ public class Space {
 		
 	}
 	
-	private void myPage() {
+	private void myBoard() {
 		
+	}
+	
+	private void printMenu() {
+		System.out.printf("[1] %s\n", log == -1 ? "회원가입" : "탈퇴");
+		System.out.printf("[2] %s\n", log == -1 ? "로그인" : "로그아웃");
+		System.out.println("[3] 내 게시판으로 이동");
+	}
+	
+	private void runMenu(int select) {
+		if(select == 1 && log == -1)
+			join();
+		else if(select == 1 && log != -1)
+			leave();
+		else if(select == 2 && log == -1)
+			login();
+		else if(select == 2 && log != -1)
+			logout();
+		else if(select == 3)
+			myBoard();
 	}
 	
 	public void run() {
 		while(true) {
-			
+			System.out.println("회원 " + um.getUserCount() + "명");
+			System.out.println("log = " + log);
+			printMenu();
+			int select = inputNumber("menu");
+			runMenu(select);
 		}
 	}
 }
